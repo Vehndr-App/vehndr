9b6fc5c6710ff5640459f28f6b069a29aa0af9ea
Kevin Smyth <>
2026-02-03 20:28:49 -0600

    intialize capacitor for mobile app builds

---
+++ android/app/src/main/java/com/vehndr/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.vehndr.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.vehndr.app',
+  appName: 'Vehndr',
+  webDir: 'out'
+};
+
+export default config;
